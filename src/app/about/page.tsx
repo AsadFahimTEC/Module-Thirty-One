05f5a94f3e9c0922bb37d6dfc862739311d102cc
AsadFahimTEC <fah485434@gmail.com>
2026-01-14 08:03:44 +0600

    static and dynamic builds finish

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,12 @@
 
+export const dynamic = 'force-dynamic';
+
 export default async function AboutPage() {
 
+     //* For simulating load time
     await new Promise((resolve) => setTimeout(resolve, 4000));
-
+    
+    //* For simulating error
     // throw new Error("Something went wrong");
 
     return (
