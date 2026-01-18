e5b411f6626bfd01aafd6d23869d7ee1c0e5d5e8
AsadFahimTEC <fah485434@gmail.com>
2026-01-18 19:42:20 +0600

    get session data in server component using fetch

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,15 +1,27 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
+  const cookieStore = await cookies();
+  console.log(cookieStore.get("better-auth.session_token"));
 
-  const session = await authClient.getSession();
+  console.log(cookieStore.toString());
 
-  console.log(session);
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
   
+  const session = await res.json();
+  console.log(session);
+
+
+
   return (
     <div>
-     <Button variant="outline">Click Here</Button>
+      <Button variant="outline">Click Here</Button>
     </div>
   );
 }
