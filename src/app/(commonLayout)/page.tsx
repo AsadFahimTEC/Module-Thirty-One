862f005b6e396e1fb4eb2dc8b144aec179d13105
AsadFahimTEC <fah485434@gmail.com>
2026-01-19 09:41:08 +0600

    fetching dummy blog posts

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { blogService } from "@/services/blog.service";
 
-export default function Home() {
+export default async function Home() {
+  const {data} = await blogService.getBlogPosts();
+  console.log(data);
 
   return (
     <div>
