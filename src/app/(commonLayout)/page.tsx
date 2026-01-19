4550ca47b705c43f5604c937d4fad510e05318f9
AsadFahimTEC <fah485434@gmail.com>
2026-01-19 08:04:09 +0600

    proxy.ts file added

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
 
-export default async function Home() {
-  const {data} = await userService.getSession();
-  console.log(data);
+export default function Home() {
+  // const {data} = await userService.getSession();
+  // console.log(data);
 
   return (
     <div>
