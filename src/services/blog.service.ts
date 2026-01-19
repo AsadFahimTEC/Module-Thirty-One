cdc18bfb62af2ed72d2dad96b068a364cc6c3670
AsadFahimTEC <fah485434@gmail.com>
2026-01-19 13:23:41 +0600

    ISR issue solve

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -2,10 +2,14 @@ import { env } from "@/env";
 
 const API_URL = env.API_URL;
 
+//* No Dynamic and No { cache: no-store } : SSG -> Static Page
+//* { cache: no-store } : SSR -> Dynamic Page
+//* next: { revalidate: 10 } : ISR -> Mix between static and dynamic
+
 export const blogService = {
     getBlogPosts: async function () {
         try {
-            const res = await fetch(`${API_URL}/posts`);
+            const res = await fetch(`${API_URL}/posts`, { next: { revalidate: 10 } } );
             const data = await res.json();
 
             // This is an example
