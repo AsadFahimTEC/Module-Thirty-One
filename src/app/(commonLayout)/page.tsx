972ebe0b10cbe01a03e77c95d44733e2e4f103bf
AsadFahimTEC <fah485434@gmail.com>
2026-01-19 09:01:02 +0600

    private routes implement using proxy.ts files

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  // const {data} = await userService.getSession();
-  // console.log(data);
 
   return (
     <div>
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
+import { userService } from "./services/user.service";
+import { Roles } from "./constants/roles";
 
 export async function proxy(request: NextRequest) {
-    console.log("Hello from proxy: ", request.url);
+    const pathName = request.nextUrl.pathname;
+    let isAuthenticated = false;
+    let isAdmin = false;
+
+    const {data} = await userService.getSession();
+
+    if(data) {
+        isAuthenticated = true;
+        isAdmin = data.user.role === Roles.admin;
+    }
+
+    //* User in not authenticated at all
+    if(!isAuthenticated){
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    //* User is authenticated and role = ADMIN
+    //* User can not visit user dashboard
+    if(isAdmin && pathName.startsWith("/dashboard")){
+        return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    }
+    //* User is authenticated and role = USER
+    //* User can not visit admin-dashboard
+    if(!isAdmin && pathName.startsWith("/admin-dashboard")){
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
 
     return NextResponse.next();
 }
 
+
 export const config = {
-    matcher: ["/dashboard"],
+    matcher: [
+        "/dashboard",
+        "/dashboard/:path*",
+        "/admin-dashboard",
+        "/admin-dashboard/:path*",
+    ],
 };
