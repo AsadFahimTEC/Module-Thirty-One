6a6a4eb791b4fa560a82be46f87c635ab8d2d289
AsadFahimTEC <fah485434@gmail.com>
2026-01-22 10:49:16 +0600

    implementing tan stack form

--- src/components/modules/authentication/register-form.tsx
+++ src/components/modules/authentication/register-form.tsx
@@ -8,7 +8,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { FieldGroup } from "@/components/ui/field";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 import { useForm } from "@tanstack/react-form";
 
 
@@ -21,7 +22,7 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
       password: "",
     },
     onSubmit: async ({ value }) => {
-      console.log("Submit Clicked");
+      console.log(value);
     },
   });
 
@@ -37,10 +38,51 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
         <form id="login-form" onSubmit={(e) => {
           e.preventDefault();
           form.handleSubmit();
-        }}> 
-        <FieldGroup>
-          <form.Field name = "name" children={ () => <Field></Field>} />
-        </FieldGroup>
+        }}>
+          <FieldGroup>
+            <form.Field name="name" children={(field) => {
+              return (
+                <Field>
+                  <FieldLabel htmlFor={field.name}>Name</FieldLabel>
+                  <Input
+                  type="text"
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                </Field>
+              );
+            }} />
+            <form.Field name="email" children={(field) => {
+              return (
+                <Field>
+                  <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                  <Input
+                  type="email"
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                </Field>
+              );
+            }} />
+            <form.Field name="password" children={(field) => {
+              return (
+                <Field>
+                  <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                  <Input
+                  type="password"
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                </Field>
+              );
+            }} />
+          </FieldGroup>
         </form>
       </CardContent>
       <CardFooter className="flex justify-end">
