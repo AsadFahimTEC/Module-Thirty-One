19434285d1f7543df6ea34126b074f74add71f6c
AsadFahimTEC <fah485434@gmail.com>
2026-01-24 09:37:33 +0600

    handling server action using client component

--- src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
+++ src/app/(dashboardLayout)/@user/dashboard/create-blog/page.tsx
@@ -1,6 +1,8 @@
 import { blogService } from "@/services/blog.service";
 import CreateBlogFormServer from "../../CreateBlog/CreateBlogFormServer";
+
 import { BlogPost } from "@/types";
+import { CreateBlogFormClient } from "../../CreateBlog/CreateBlogFormClient";
 
 export default async function CreateBlogPage() {
     const { data } = await blogService.getBlogPosts({}, {cache: "no-store"});
@@ -8,7 +10,8 @@ export default async function CreateBlogPage() {
     console.log(data);
     return (
         <div>
-            <CreateBlogFormServer></CreateBlogFormServer>
+            {/* <CreateBlogFormServer></CreateBlogFormServer> */}
+            <CreateBlogFormClient></CreateBlogFormClient>
             {
                 data.data.map((item: BlogPost) => (
                     <p key={item.id}>{item.title}</p>
