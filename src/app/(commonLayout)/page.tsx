301eea037fa3de129a6d2551ebb2082bca8b616d
AsadFahimTEC <fah485434@gmail.com>
2026-01-24 20:58:34 +0600

    next.js image component added

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,8 @@
 import BlogCard from "@/components/modules/homepage/BlogCard";
 import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types";
+import coffee from "../../../public/images/coffee.jpg";
+import Image from "next/image";
 
 
 export default async function Home() {
@@ -30,25 +32,26 @@ export default async function Home() {
     <div className="max-w-7xl mx-auto px-4">
       <div className="mb-12 mt-8 h-[calc(100vh-80px)] flex flex-col justify-center">
         <div className="relative w-full h-96 mb-6">
-          <img
+          {/* <img
             src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=1920&q=100"
             alt="Hero"
             className="w-full h-full object-cover rounded-lg"
-          />
-
-          {/* <Image
-            src={coffee}
-            fill
-            alt="Hero"
-            className="object-cover rounded-md"
           /> */}
-          {/* <Image
+
+          {/* <Image 
+          src = {coffee} 
+          fill 
+          alt ="Hero" 
+          className="object-cover rounded-md" 
+          /> */}
+
+          <Image
             src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=1920&q=100"
             fill
             priority
             alt="Hero"
             className="object-cover rounded-md"
-          /> */}
+          />
         </div>
         <h1 className={"text-5xl font-bold text-center mb-4"}>
           Welcome to Our Blog
